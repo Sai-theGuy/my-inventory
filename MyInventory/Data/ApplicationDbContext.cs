526525e71e6282c2e46f5aa9aab241f4f8224884
Simon Philip Estrada <12019609@benilde.edu.ph>
2022-03-21 22:29:06 +0800

    Initial Commit

---
+++ MyInventory/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+using MyInventory.Models;
+
+namespace MyInventory.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ MyInventory/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyInventory.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
