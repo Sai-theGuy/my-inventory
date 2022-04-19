73228177699780dbe3de02c9d0150850e1a2c046
Sai_theGuy <simonphilip.estrada.edu.ph>
2022-04-19 17:24:02 +0800

    ContactForm Fix

--- MyInventory/Controllers/HomeController.cs
+++ MyInventory/Controllers/HomeController.cs
@@ -32,6 +32,11 @@ namespace LifeLine.Controllers
             return View();
         }
 
+        public IActionResult ContactForm()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
