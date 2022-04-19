29ea2cd0917f92b0cd0ea3335a8425f1993529e9
anjocortez <joseantonio.cortez@benilde.edu.ph>
2022-04-19 17:24:28 +0800

    Fixed the ContactForm

--- MyInventory/Controllers/HomeController.cs
+++ MyInventory/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace LifeLine.Controllers
         {
             return View();
         }
+        public IActionResult ContactForm()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
