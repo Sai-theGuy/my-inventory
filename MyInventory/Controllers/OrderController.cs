f1d522e22f3f327902688e6379956f20fa70dc5c
anjocortez <joseantonio.cortez@benilde.edu.ph>
2022-04-20 05:29:23 +0800

    Added Printable Receipt (Incomplete)

---
+++ MyInventory/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LifeLine.Models;
+using LifeLine.Data;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+
+
+
+
+namespace LifeLine.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Receipt()
+        {
+            var order = _context.PurchaseOrders.ToList();
+            return View()
+        }
+    }
+}
