8a062d83b9c8843637e7c4117100597afac979b9
Sai_theGuy <simonphilip.estrada.edu.ph>
2022-04-20 16:32:21 +0800

    Added Auditing Features

---
+++ Audit (Tables and Triggers)/ProductAuditTrigger.sql
@@ -0,0 +1,53 @@
+USE [LifelineMedical(Actual)]
+GO
+
+CREATE TRIGGER TriggerProductInsert ON ProductListings
+AFTER INSERT
+AS
+	INSERT ProductListingAudit
+	SELECT 
+		ListingID
+		, GETDATE()
+		, NULL
+		, 'Insert'		
+		, Price
+		, Description
+		, Type
+		, StocksLeft
+		, UnitMeasurement
+	FROM inserted
+GO
+
+CREATE TRIGGER TriggerProductUpdate ON ProductListings
+AFTER UPDATE
+AS
+	INSERT ProductListingAudit
+	SELECT
+		a.ListingID
+		, NULL	
+		, GETDATE()
+		, 'Update'
+		, b.Price
+		, b.Description
+		, b.Type
+		, b.StocksLeft
+		, b.UnitMeasurement			
+	 FROM deleted a JOIN inserted b ON a.ListingID = b.ListingID
+GO
+
+CREATE TRIGGER TriggerProductDelete ON ProductListings
+AFTER DELETE
+AS
+	INSERT ProductListingAudit
+	SELECT 
+		ListingID
+		, NULL
+		, GETDATE()
+		, 'Delete'		
+		, Price
+		, Description
+		, Type
+		, StocksLeft
+		, UnitMeasurement
+	FROM deleted
+GO
